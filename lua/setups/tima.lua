6bb903dc93f094d6f3b3b2e36f481845024d4df1
Kain (Pseudonym) <88363187+EKQRCalamity@users.noreply.github.com>
2024-05-31 16:18:40 +0200

    Update tima.lua

--- lua/setups/tima.lua
+++ lua/setups/tima.lua
@@ -1,51 +1,60 @@
-local TIME = {}
+local TIMA = {}
 
-local function get_time()
-  return { os.date("%H:%M:%S") }
+TIMA.setup = function(posX, posY, border)
+  local function get_time()
+    return { os.date("%H:%M:%S") }
+  end
+
+  local function update_time(buf)
+    local ascii_time = get_time()
+    vim.api.nvim_buf_set_lines(buf, 0, -1, false, ascii_time)
+  end
+
+
+  local buf, _ = TIMA.setup_window(posX, posY, border)
+
+  update_time(buf)
+
+  local timer = vim.loop.new_timer()
+  timer:start(250, 250, vim.schedule_wrap(function()
+    update_time(buf)
+  end))
 end
 
-local function create_win(pos)
+TIMA.setup_window = function (posX, posY, border)
   local buf = vim.api.nvim_create_buf(false, true)
-  local width = 8;
-  local cols = {
-    ["right"] = vim.o.columns,
-    ["center"] = (vim.o.columns / 2) - ((width / 2) + 1),
-    ["left"] = 0
-  }
-  local col
-  if (cols[pos]) then
-    col = cols[pos]
-  else
-    col = vim.o.columns - (width / 2) - 1
+  local width = 8
+  local height = 1
+
+  local col = 1000
+  local row = 0
+
+  if (border == nil) then
+    border = "single"
   end
+
+  if (posX == "center") then
+    col = vim.o.columns - ((vim.o.columns + width) / 2)
+  elseif (posX == "left") then
+    col = 1
+  end
+
+  if (posY == "center") then
+    row = vim.o.lines - ((vim.o.lines + height) / 2)
+  elseif (posY == "bottom" or posY == "bot") then
+    row = vim.o.lines - height - 1
+  end
+
   local win = vim.api.nvim_open_win(buf, false, {
     relative = "editor",
     width = width,
-    height = 1,
-    row = 0,
+    height = height,
+    row = row,
     col = col,
     style = "minimal",
-    border = "single"
+    border = border,
   })
   return buf, win
 end
 
-local function update_time(buf)
-  local time = get_time()
-  vim.api.nvim_buf_set_lines(buf, 0, -1, false, time)
-end
-
-TIME.setup = function(pos)
-  local buf, _ = create_win(pos)
-  update_time(buf)
-
-  local timer = vim.loop.new_timer()
-
-  timer:start(500, 500,
-    vim.schedule_wrap(function()
-      update_time(buf)
-    end
-  ))
-end
-
-return TIME
+return TIMA;
